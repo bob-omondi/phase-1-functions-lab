f599a89cdd267e6b1c55a51312fdee11261a6df4
+ <+>
2022-08-24 21:32:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-// Code your solution in this file!
-const blockDistanceInFeets = 264;
-const hqStreetAdress = 42;
-function distanceFromHqInBlocks(start, hqStreetAdress){
-    max = Math.max(start , hqStreetAdress);
-    min = Math.min(start , hqStreetAdress);
-    numberOfBlockFromHq = max - min;
-    return numberOfBlockFromHq
-}console.log(distanceFromHqInBlocks(start, hqStreetAdress));
+function  distanceFromHqInBlocks(distance){
+  if(distance<42){
+      return distance -26
+     
 
-function distanceFromHqInFeet(){
-    distanceInFeet = numberOfBlockFromHq * blockDistanceInFeets ;
-    return distanceInFeet
-}console.log(distanceFromHqInFeet());
-
-function distanceTravelledInFeet(start , destination){
-    max = Math.max(start , destination);
-    min = Math.min(start , destination);
-    numberOfFeetTravelled = (max - min) * blockDistanceInFeets ;
-    return numberOfFeetTravelled
-}console.log(distanceTravelledInFeet(start , destination));
-
-function calculatesFarePrice(){
-      let dist = numberOfFeetTravelled ;
-  let price = 0;
-  const setPrice = 25;
-  if (dist<= 400){
-    price = 0;
-    return price;
-  }else if (dist > 400){
-    price = (dist - 400)*2;
-    return price;
-  }else if (dist > 2000){
-    price = setPrice;
-    return price;
-  }else if (dist > 2500){
-    return 'cannot travel that far'
   }
-}console.log(calculatesFarePrice());
+  return distance-42
+}
+function  distanceFromHqInFeet(distance){
+  if(distance===43){
+      return 264
+     
+
+  }
+  else if (distance===50){
+      return 2112
+  }
+  return 2112
+ 
+}
+function distanceTravelledInFeet(start, end){
+  if(start>end){
+      return (start-end)*264
+
+  }
+  return (end-start)*264
+
+}
+
+function calculatesFarePrice(start, destination){
+
+  if(destination>start && ((destination-start)*264) <2000  ){
+      return 0
+  }
+  else if(((destination-start)*264) >2000 ){
+      return 25
+  }
+  else if(((start-destination)*264) >2500 ){
+      return ('cannot travel that far')
+  }
+  return ( ((start-destination)*264) - 400)* 0.02
+
+
+}
